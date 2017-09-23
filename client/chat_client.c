b7c6b703545b663d7c55e405573e0f9a04afe5e6
Alkesst <alec.augal@gmail.com>
2017-09-23 16:31:21 +0200

    Shows the current username in the client while writing. Made a struct to handle with errors with this new feature.

--- client/chat_client.c
+++ client/chat_client.c
@@ -11,14 +11,19 @@
 #include <sys/socket.h>
 // Client Main
 
+struct Client{
+    int socket;
+    char* username;
+    pthread_t thread;
+};
 
 static void* receive_and_print_message(void* polymorph);
-static void read_and_send_message(int socket);
-static int set_ip_and_port(struct in_addr* ip_adress, int port, pthread_t* client_thread);
+static void read_and_send_message(struct Client* client);
+static int set_ip_and_port(struct in_addr* ip_adress, int port, struct Client* client);
 
 
 int main(int argc, const char* argv[]){
-    pthread_t client_thread;
+    struct Client client;
     struct in_addr ip_address;
     int port = 3128;
     if(argc > 1){
@@ -39,9 +44,9 @@ int main(int argc, const char* argv[]){
         fprintf(stderr, "Argument is requiered: Expected IP address\n");
         exit(3);
     }
-    int socket = set_ip_and_port(&ip_address, port, &client_thread);
+    int socket = set_ip_and_port(&ip_address, port, &client);
     if(socket != -1){
-        read_and_send_message(socket);
+        read_and_send_message(&client);
     } else{
         perror("");
         exit(4);
@@ -50,34 +55,38 @@ int main(int argc, const char* argv[]){
     return 0;
 }
 
-static int set_ip_and_port(struct in_addr* ip_address, int port, pthread_t* client_thread){
+static int set_ip_and_port(struct in_addr* ip_address, int port, struct Client* client){
     /*Creates the socket and stablish the connection with the server given. Returns -1 if there's any issue*/
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(client_socket != -1){
+    client->socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(client->socket != -1){
         struct sockaddr_in server_address;
         server_address.sin_family = AF_INET;
         server_address.sin_addr = *ip_address;
         server_address.sin_port = htons(port);
-        if(connect(client_socket, (struct sockaddr*) &server_address, sizeof(struct sockaddr_in)) == -1){
-            close(client_socket);
-            client_socket = -1;
+        if(connect(client->socket, (struct sockaddr*) &server_address, sizeof(struct sockaddr_in)) == -1){
+            close(client->socket);
+            client->socket = -1;
         } else{
-            pthread_create(client_thread, NULL, receive_and_print_message, (void*) (long) client_socket);
+            pthread_create(&client->thread, NULL, receive_and_print_message, &client->socket);
         }
     }
-    return client_socket;
+    return client->socket;
 }
 
 static void* receive_and_print_message(void* polymorph){
-    int client_socket = (int) (long) polymorph;
+    struct Client* client = polymorph;
     char* message = malloc(1000);
     while(true){
-        int bytes_readed = recv(client_socket, message, 1000, 0);
-        if(client_socket == -1){
+        int bytes_readed = recv(client->socket, message, 1000, 0);
+        if(client->socket == -1){
             free(message);
             pthread_exit(NULL);
         } else if(bytes_readed > 0){
             printf("%.*s", bytes_readed, message);
+            if(strstr(message, "This username already exists") != NULL){
+                free(client->username);
+                client->username = NULL;
+            }
         } else{
             printf("Server has closed...\nPress intro to close this process...\n");
             free(message);
@@ -87,16 +96,23 @@ static void* receive_and_print_message(void* polymorph){
     }
 }
 
-static void read_and_send_message(int socket){
+static void read_and_send_message(struct Client* client){
     size_t length;
+    client->username = NULL;
     char* message = NULL;
     while(!feof(stdin) && !ferror(stdin)){
         if(getline(&message, &length, stdin) != -1){
             length = strlen(message);
-            send(socket, message, length, 0);
+            send(client->socket, message, length, 0);
+            if(client->username == NULL){
+                client->username = strdup(message);
+                client->username[length - 1] = '\0';
+            }
+            printf("(%s): ", client->username);
             free(message);
             message = NULL;
         }
     }
-    close(socket);
+    free(client->username);
+    close(client->socket);
 }
